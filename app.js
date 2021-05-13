350fdc3c545bfabd3f30e8e63a73bc8b7e7e084a
unknown <jesstanks87@gmail.com>
2021-05-13 19:54:34 +0800

    First Version

---
+++ app.js
@@ -0,0 +1,35 @@
+// Imports
+const express = require('express')
+
+const fs = require('fs');
+const app = express()
+const port = process.env.PORT || 3000;
+
+let jsonData = require('./data.json');
+// Static Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
+
+// Set Views
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.get('', (req, res) => {
+    res.render('index', { text: 'This is EJS'})
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', { text: 'About Page'})
+})
+
+app.get('/data', (req, res) => {
+	res.json(jsonData);
+})
+
+
+
+
+//  Listen on port 3000
+app.listen(port, () => console.info(`Listening on port ${port}`))
